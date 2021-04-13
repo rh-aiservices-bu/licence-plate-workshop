13242d3fc9f4671a468abfd6e9feacc80ea85edd
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-04-13 16:31:57 -0400

    cleanup branch

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-
-forwarded_allow_ips = '*'
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
