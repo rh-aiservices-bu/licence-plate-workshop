ca42f42b15de0580d453ff9f3ae4e312fa5be7f1
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-04-23 17:10:34 -0400

    app preparation

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '240'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
